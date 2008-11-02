e08a9e921aaca22382036eee91729860e50ec95e
ianhaywood3 <ianhaywood3@9e6bf6c3-1a51-0410-b0be-bffa830d3d59>
2008-11-02 06:01:31 +0000

    actually forget about this one. But my new script is better

--- trunk/wedgetail/rails/wedgetail/script/reload_mongrel.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-kill `cat tmp/pids/mongrel.8000.pid`
-kill `cat tmp/pids/mongrel.8001.pid`
-kill `cat tmp/pids/mongrel.8002.pid`
-/usr/bin/ruby1.8 /usr/bin/mongrel_rails start -d -e development -p 8000 -P tmp/pids/mongrel.8000.pid -l log/mongrel.8000.log
-/usr/bin/ruby1.8 /usr/bin/mongrel_rails start -d -e development -p 8001 -P tmp/pids/mongrel.8001.pid -l log/mongrel.8001.log
-/usr/bin/ruby1.8 /usr/bin/mongrel_rails start -d -e development -p 8002 -P tmp/pids/mongrel.8002.pid -l log/mongrel.8002.log
