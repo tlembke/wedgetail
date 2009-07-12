b521f716303216b4a01fcf1eb4f4140096dd5840
tlembke <tlembke@9e6bf6c3-1a51-0410-b0be-bffa830d3d59>
2009-07-12 18:39:49 +0000

    Change result_ref to request_set

--- trunk/wedgetail/rails/wedgetail/app/models/result_ticket.rb
+++ trunk/wedgetail/rails/wedgetail/app/models/result_ticket.rb
@@ -1,5 +1,5 @@
 class ResultTicket < ActiveRecord::Base
-  validates_uniqueness_of :result_ref, 
+  validates_uniqueness_of :request_set, 
                           :on => :create, 
                           :message => "is already being used"
   validates_uniqueness_of :ticket, 
