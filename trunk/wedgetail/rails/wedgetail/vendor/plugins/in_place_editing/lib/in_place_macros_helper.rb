8c6c506fe7db67464a35a08975a5c3fe18a09011
tlembke <tlembke@9e6bf6c3-1a51-0410-b0be-bffa830d3d59>
2010-11-05 22:53:25 +0000

    Calendar Date Select Plug In

--- trunk/wedgetail/rails/wedgetail/vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
+++ trunk/wedgetail/rails/wedgetail/vendor/plugins/in_place_editing/lib/in_place_macros_helper.rb
@@ -67,6 +67,39 @@ module InPlaceMacrosHelper
     javascript_tag(function)
   end
   
+  def in_place_delete_editor(field_id, options = {})
+    function =  "new Ajax.InPlaceDeleteEditor("
+    function << "'#{field_id}', "
+    function << "'#{url_for(options[:url])}'"
+
+    js_options = {}
+
+    if protect_against_forgery?
+      options[:with] ||= "Form.serialize(form)"
+      options[:with] += " + '&authenticity_token=' + encodeURIComponent('#{form_authenticity_token}')"
+    end
+
+    js_options['cancelText'] = %('#{options[:cancel_text]}') if options[:cancel_text]
+    js_options['okText'] = %('#{options[:save_text]}') if options[:save_text]
+    js_options['loadingText'] = %('#{options[:loading_text]}') if options[:loading_text]
+    js_options['savingText'] = %('#{options[:saving_text]}') if options[:saving_text]
+    js_options['rows'] = options[:rows] if options[:rows]
+    js_options['cols'] = options[:cols] if options[:cols]
+    js_options['size'] = options[:size] if options[:size]
+    js_options['externalControl'] = "'#{options[:external_control]}'" if options[:external_control]
+    js_options['loadTextURL'] = "'#{url_for(options[:load_text_url])}'" if options[:load_text_url]        
+    js_options['ajaxOptions'] = options[:options] if options[:options] 
+    js_options['htmlResponse'] = !options[:script] if options[:script]
+    js_options['callback']   = "function(form) { return #{options[:with]} }" if options[:with]
+    js_options['clickToEditText'] = %('#{options[:click_to_edit_text]}') if options[:click_to_edit_text]
+    js_options['textBetweenControls'] = %('#{options[:text_between_controls]}') if options[:text_between_controls]
+    function << (', ' + options_for_javascript(js_options)) unless js_options.empty?
+    
+    function << ')'
+
+    javascript_tag(function)
+  end
+  
   # Renders the value of the specified object and method with in-place editing capabilities.
   def in_place_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
     instance_tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
@@ -82,5 +115,21 @@ module InPlaceMacrosHelper
     end
     
     return tag + in_place_editor(tag_options[:id], in_place_editor_options)
+  end
+  
+  def in_place_delete_editor_field(object, method, tag_options = {}, in_place_editor_options = {})
+    instance_tag = ::ActionView::Helpers::InstanceTag.new(object, method, self)
+    tag_options = {:tag => "span",
+                   :id => "#{object}_#{method}_#{instance_tag.object.id}_in_place_editor",
+                   :class => "in_place_editor_field"}.merge!(tag_options)
+    in_place_editor_options[:url] = in_place_editor_options[:url] || url_for({ :action => "set_#{object}_#{method}", :id => instance_tag.object.id })
+    if in_place_editor_options[:type]=="simple"
+       tag = content_tag(tag_options.delete(:tag), simple_format(instance_tag.value(instance_tag.object)).sub(/\A<p>/,""),tag_options)
+    #tag = content_tag(tag_options.delete(:tag), h(instance_tag.value(instance_tag.object)),tag_options)
+    else
+      tag = content_tag(tag_options.delete(:tag), h(instance_tag.value(instance_tag.object)),tag_options)
+    end
+    
+    return tag + in_place_delete_editor(tag_options[:id], in_place_editor_options)
   end
 end
